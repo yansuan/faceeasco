30ae690aef0f459345dc613590779654776a930a
yansuan <89984142@qq.com>
2021-07-02 20:41:32 +0800

    fix record and stranger roomtemperature data type

--- http_api_stranger.go
+++ http_api_stranger.go
@@ -6,16 +6,16 @@ type HttpStrangerResponse struct {
 }
 
 type HttpStrangerData struct {
-	UserId          string `json:"user_id"`    //
-	RecogType       string `json:"recog_type"` //
-	RecogTime       string `json:"recog_time"` //
-	Photo           string `json:"photo"`      //
-	Gender          int8   `json:"gender"`     //
-	UserName        string `json:"user_name,omitempty"`
-	BodyTemperature string `json:"body_temperature,omitempty"`
-	Confidence      string `json:"confidence,omitempty"`
-	Reflectivity    int    `json:"reflectivity,omitempty"`
-	RoomTemperature string `json:"room_temperature,omitempty"`
+	UserId          string  `json:"user_id"`    //
+	RecogType       string  `json:"recog_type"` //
+	RecogTime       string  `json:"recog_time"` //
+	Photo           string  `json:"photo"`      //
+	Gender          int8    `json:"gender"`     //
+	UserName        string  `json:"user_name,omitempty"`
+	BodyTemperature string  `json:"body_temperature,omitempty"`
+	Confidence      string  `json:"confidence,omitempty"`
+	Reflectivity    int     `json:"reflectivity,omitempty"`
+	RoomTemperature float32 `json:"room_temperature,omitempty"`
 	//HealthTreasureCode string `json:"health_treasure_code,omitempty"`
 	Extra             string `json:"extra,omitempty"`
 	CardNumber        string `json:"card_number,omitempty"`
