0abcdf1bdc081b88596ce7483f6f35383452e540
yansuan <89984142@qq.com>
2021-07-17 10:02:08 +0800

    add user del multi

--- websocket_api_user_del_multi.go
+++ websocket_api_user_del_multi.go
@@ -1,1 +1,28 @@
 package faceeasco
+
+type WebsocketApiUserDelMultiRequest struct {
+	WebsocketRequestHeader
+	Data WebsocketApiUserDelMultiRequestData `json:"data"`
+}
+
+type WebsocketApiUserDelMultiRequestData struct {
+	WebsocketRequestData
+	UserIds  []string `json:"user_ids"`  //
+	UserType int      `json:"user_type"` //
+}
+
+type WebsocketApiUserDelMultiResponse struct {
+	WebsocketResponseHeader
+	Data WebsocketApiUserDelMultiDataResponse `json:"data"`
+}
+
+type WebsocketApiUserDelMultiDataResponse struct {
+	WebsocketResponseData
+	Failed []WebsocketApiUserDelMultiFailedResponse `json:"delFailed"`
+}
+
+type WebsocketApiUserDelMultiFailedResponse struct {
+	UserId string `json:"user_id"` //
+	Code   int    `json:"code"`    //0成功，其它失败
+	Msg    string `json:"msg"`     //提示信息
+}
